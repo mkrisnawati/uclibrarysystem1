12017c8d4fdfd6d2f3500351f5d7915bb26877f5
Melisa Krisnawati <50088776+mkrisnawati@users.noreply.github.com>
2022-05-08 13:33:02 +0700

    Add files via upload

---
+++ denied.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<meta charset=utf-8" />
+<title>Login UC Library</title>
+<meta name="description" content="Simple e-Perpustakaan">
+<link href="Bootstrap/css/style.css" rel="stylesheet" type="text/css" />
+<link href="Bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" href="images/uc.jpg" />
+</head>
+
+<body>
+<div class="clear">&nbsp;</div>
+<form id="form1" name="form1" method="POST" action="<?php echo $loginFormAction; ?>">
+  <table width="550" height="458" border="0" align="center" cellpadding="2" background="images/uclibrarylogin.jpeg">
+  <tr>
+      <td width="218" height="150">&nbsp;</td>
+    </tr>
+    <tr>
+      <td height="30" align="center"><b>Akses Ditolak</b></td>
+    </tr>
+    <tr>
+      <td height="30" align="center">Anda belum login, atau anda tidak berhak dengan halaman ini, silakan hubungi administrator.
+      <a href="login.php">Login</a>
+      </td>
+    </tr>
+  </table>
+    </form>
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,5 @@
+<h2>Dashboard</h2>
+<div class="well">
+<p>Selamat datang <b><?php echo $_SESSION['MM_Username']; ?></b> di Sistem Informasi Universitas Ciputra. </p>
+<p>Sistem ini digunakan untuk mempermudah segala proses yang dilakukan pada Perpustakaan Universitas Ciputra, baik rekapitulasi, pencarian maupun peminjaman. </p>
+</div>
